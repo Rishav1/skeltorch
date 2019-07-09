15633ee9b391efeabdb45cf43a5b7b8c644eeffe
David Álvarez de la Torre <davidalvarezdlt@gmail.com>
2019-07-09 22:48:49 +0200

    Added Comments

--- pytorch_skeleton/model.py
+++ pytorch_skeleton/model.py
@@ -2,9 +2,30 @@ import torch.nn
 
 
 class SkeletonModel(torch.nn.Module):
+    """Definition of the project model.
 
-    def __init__(self):
-        pass
+    Defines the architecture of the neural network used in the project as a child class of `torch.nn.Module`.
+    """
+    param: int
+
+    def __init__(self, param: int):
+        """Constructor of the neural network.
+
+        Args:
+            param (int): hyperparameter required by the layers of the neural network.
+        """
+        super(SkeletonModel, self).__init__()
+        self.param = param
 
     def forward(self, input_data):
+        """Performs a forward pass through the neural network.
+
+        Performs a forward pass throught the entire model used in the project.
+
+        Args:
+            input_data (torch.Tensor): input of the neural network.
+
+        Returns:
+            torch.Tensor: Output obtained after propagating the input through the neural network.
+        """
         pass
--- pytorch_skeleton/utils.py
+++ pytorch_skeleton/utils.py
@@ -2,9 +2,22 @@ import logging
 
 
 class SkeletonUtils:
+    """Class to store auxiliar functions.
+
+    Stores auxiliar project functionality as static methods.
+    """
 
     @staticmethod
     def get_logger(log_file, verbose=False):
+        """Get a logger object to be used in the execution.
+
+        Args:
+            log_file: path to the log file where the logging is stored.
+            verbose: whether or not to output log information through the terminal.
+
+        Returns:
+            An instace of a `logging.Logger` object properly configurated.
+        """
         logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] %(message)s',
                             datefmt='%Y-%m-%d %H:%M:%S')
         logger = logging.getLogger('glow')
